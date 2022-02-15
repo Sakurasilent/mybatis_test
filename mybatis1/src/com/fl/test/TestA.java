89afa5253d586d8cd5ce157190e68881e9045f2e
Sakurasilent <492627639@qq.com>
2022-02-15 21:16:08 +0800

    mybatis 小练习

---
+++ mybatis1/src/com/fl/test/TestA.java
@@ -0,0 +1,25 @@
+package com.fl.test;
+
+import com.fl.entity.Flower;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class TestA {
+    public static void main(String[] args) throws IOException {
+        //解析Mybatis.xml
+        InputStream inputStream = Resources.getResourceAsStream("mybatis.xml");
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = factory.openSession();
+        List<Flower> list = sqlSession.selectList("a.b.selectAll");
+        System.out.println(list);
+        //关闭资源
+        sqlSession.close();
+
+    }
+}
